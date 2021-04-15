ce39e180f7d6a7f3403521a1e0ccf61b4343d51e
Kévin KNITTEL <knittel.kevin@outlook.fr>
2021-04-15 12:27:15 +0200

    Panier OK sauf suppression d'un article

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,4 +1,10 @@
+import { RemoveArticle } from './../../shared/actions/panier.action';
 import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Article } from 'src/shared/models/article';
+import { PanierState } from 'src/shared/states/panier-state';
+import { ArticleService } from '../service/article.service';
 
 @Component({
   selector: 'app-panier',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanierComponent implements OnInit {
 
-  constructor() { }
+  nbElementPanier: any;
+
+  panierElements$!: Observable<Article[]>;
+
+  constructor(private articleService: ArticleService,
+    private store: Store) { }
 
   ngOnInit(): void {
+    this.panierElements$ = this.store.select(state => state.panier.panier);
   }
 
+  delReference(ref: string) {
+    // À corriger : il supprime tous les articles avec la ref, il faut supp par rapport à l'index dans la liste
+    // this.store.dispatch(new RemoveArticle(ref));
+  }
 }
