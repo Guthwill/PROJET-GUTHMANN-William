ea34ad07b4afedf5d4d963d5fffafcac84696a80
wguthmann <williamguthmann@gmail.com>
2021-04-27 20:40:13 +0200

    update

--- src/app/api-http-interceptor.ts
+++ src/app/api-http-interceptor.ts
@@ -3,53 +3,48 @@ import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
 import { Injectable } from '@angular/core';
 import { Observable, of, pipe } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ApiHttpInterceptor implements HttpInterceptor {
 
-  jwtToken: String = "";
+  token: String = '';
+  constructor(private router: Router) {}
 
-  constructor(private userService: UserService) { }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    if (this.jwtToken != "") {
-      req = req.clone({ setHeaders: { Authorization: `Bearer ${this.jwtToken}` } });
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (this.token != '') {
+      req = req.clone({
+        setHeaders: { Authorization: `Bearer ${this.token}` },
+      });
     }
 
-    return next.handle(req).pipe(tap(
-      (evt: HttpEvent<any>) => {
-        if (evt instanceof HttpResponse) {
-          let tab: Array<String>;
-          let enteteAuthorization = evt.headers.get("Authorization");
-
-          if (enteteAuthorization != null) {
-            tab = enteteAuthorization.split(/Bearer\s+(.*)$/i);
-
-            if (tab.length > 1) {
-              this.jwtToken = tab[1];
+    return next.handle(req).pipe(
+      tap(
+        (evt: HttpEvent<any>) => {
+          if (evt instanceof HttpResponse) {
+            let tab: Array<String>;
+            let enteteAuthorization = evt.headers.get('Authorization');
+            if (enteteAuthorization != null) {
+              tab = enteteAuthorization.split(/Bearer\s+(.*)$/i);
+              if (tab.length > 1) {
+                this.token = tab[1];
+              }
             }
           }
+        },
+        (error: HttpErrorResponse) => {
+          switch (error.status) {
+            case 401:
+              this.router.navigate(['/connexion']);
+              break;
+            default:
+              console.log('ERROR !!!!!');
+          }
         }
-
-        // (error: HttpErrorResponse) => {
-        //   switch (error.status) {
-        //     case 400:
-        //     case 401:
-        //   }
-
-        //   return of(null);
-        // }
-      }));
+      )
+    );
   }
-
-  // intercept(req: HttpRequest<any>, next: HttpHandler) {
-  //   let tokenizedReq = req.clone({
-  //     setHeaders: {
-  //       Authorization: `Bearer ${this.userService.getToken()}`
-  //     }
-  //   })
-
-  //   return next.handle(tokenizedReq)
-  // }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: '/assets/mock/bouchon.json',
+  baseUrl: 'https://projet-guthmann-william.herokuapp.com',
 };
 
 /*
